543225125c285c5335bb032444b6c1771c37d379
viral.gajera218 <viral.gajera218@gmail.com>
2025-07-23 14:21:06 +0000

    build removed

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,11 @@ const repoName = "step-campus"
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  distDir: 'docs',
-  basePath:`/${repoName}`,
-  assetPrefix:`/${repoName}`,
-  images: { unoptimized: true }
+  // output: 'export',
+  // distDir: 'docs',
+  // basePath:`/${repoName}`,
+  // assetPrefix:`/${repoName}`,
+  // images: { unoptimized: true }
 };
 
 export default nextConfig;
